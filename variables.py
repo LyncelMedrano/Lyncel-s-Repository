6e59d0e2014157041a48c70bd4518b324afa72f6
LyncelMedrano <114576244+LyncelMedrano@users.noreply.github.com>
2022-09-28 18:04:08 +0800

    Variables

---
+++ variables.py
@@ -0,0 +1,57 @@
+print("Variables")
+
+print()
+
+x = 5
+
+print("Number:", x)
+
+y = "Lyncel"
+
+print("String:", y)
+
+x = 15
+
+print("New Number:", x)
+
+y = "Medrano"
+
+print("New String:", y)
+
+print()
+
+print("Case-Sensitive.")
+
+a = 10
+A = "Hello"
+
+print("a =", a)
+print("A =", A)
+
+print("Variable Names.")
+print()
+
+print("Example:")
+
+sample_name = "sample_name"
+sampleName = "sampleName"
+SampleName = "AlphaNumeric"
+
+print("snake_case =", sample_name)
+print("camelCase =", sampleName)
+print("CapWords =", SampleName)
+
+print()
+
+print("Assign multiple values.")
+
+x, y, z = "Morning", "Afternoon", "Evening"
+print(x, y, z)
+
+print()
+
+x = y = z = "Morning Sunshine"
+print(x)
+print(y)
+print(z)
+
